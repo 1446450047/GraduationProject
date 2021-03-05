7bf0f76cb74b4ebb26a9ada7f759e9e43ebc95ee
1446450047 <1446450047@qq.com>
2021-03-05 08:53:13 +0800

    将å¤分割音频的过程分装成函数

--- code/test.py
+++ code/test.py
@@ -1,39 +1,49 @@
 from pydub import AudioSegment
+import os
 
-# 读取音频文件
-song = AudioSegment.from_file("../resources/voices/testVoice.aac", format="aac")
+def split_voice(voice_path:str,split_seconds:int ,save_path: str):
+    """
+    :param voice_path: 传入的音频资源路径
+    :param split_seconds: 分割秒数（最大为60s）
+    :param save_path: 保存路径
+    :return: void
+    """
+    # 删除split_voice中的所有文件
+    path = "split_voice"
+    for filename in os.listdir(path):
+        os.remove(path + "\\" + filename)
 
-# 获取音频的长度
-song_len = int(song.duration_seconds)
+    # 获取音频的格式
+    file_format = os.path.splitext(voice_path)[-1][1:]
 
-# 起使截取时间
-start = 0
+    # 读取音频文件
+    song = AudioSegment.from_file(voice_path, format=file_format)
 
-# 终止截取时间
-end = 60 * 1000
+    # 获取音频的长度
+    song_len = int(song.duration_seconds)
 
-# 每次截取60秒
-sixty_seconds = 60 * 1000
+    # 起使截取时间
+    start = 0
 
-while end < song_len * 1000:
-    # 截取60秒然后保存成为 .wav格式的文件
-    sixty_seconds_voice = song[start:end]
-    sixty_seconds_voice.export(str(int(start/1000)) + "_to_" + str(int(end/1000)) + "_voice.wav",format="wav")
-    start = end
-    end = start + sixty_seconds
+    # 处理每次截取时间，最大为60s
+    split_seconds = split_seconds * 1000 if split_seconds <= 60 else 60 * 1000
 
-# 截取最后的音频
-if end > song_len * 1000:
-    sixty_seconds_voice = song[start:song_len]
-    sixty_seconds_voice.export(str(int(start/1000)) + "_to_" + str(int(song_len)) + "_voice.wav",format="wav")
+    # 终止截取时间
+    end = split_seconds
+
+    while end < song_len * 1000:
+        # 截取60秒然后保存成为 .wav格式的文件(方便调用API处理)
+        sixty_seconds_voice = song[start:end]
+        sixty_seconds_voice.export(
+            save_path + str(int(start / 1000)) + "_to_" + str(int(end / 1000)) + "_voice.wav", format="wav")
+        start = end
+        end = start + split_seconds
+
+    # 截取最后的音频
+    if end >= song_len * 1000:
+        sixty_seconds_voice = song[start:song_len]
+        sixty_seconds_voice.export(
+            save_path + str(int(start / 1000)) + "_to_" + str(int(song_len)) + "_voice.wav", format="wav")
 
 
-
-
-# first_10_seconds = song[:sixty_seconds]
-#
-# last_5_seconds = song[-5000:]
-
-# print(first_10_seconds.duration_seconds)
-
-# first_10_seconds.export("first_ten_seconds.mp3", format="mp3")
+split_voice("../resources/voices/testVoice.aac",50,"./split_voice/")
